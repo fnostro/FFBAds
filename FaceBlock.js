2f6d91c9593792ee326e4d5d512acf92e08ae15d
fnostro <fnostro@users.noreply.github.com>
2017-05-08 05:07:31 -0400

    Add files via upload

--- FaceBlock.js
+++ FaceBlock.js
@@ -2,110 +2,118 @@
 var CountOfBlocked = 0;
 
 var BlockableHyperFeedItems = {
-	SponsoredAds: true,
-	MemoriesOfFriends: true
+    SponsoredAds: true,
+    Memories: true,
+    AgoToday: null
 };
 
 var BlockedItemCounts = {
-	Ads: 0,
-	Memories: 0
+    SponsoredAds: 0,
+    Memories: 0,
+    AgoToday: 0 
 };
 
-(function AdObserver(AdHandler) {
-	this.target = document;
-	this.options = {
-		childList: true,
-		subtree: true
-	};
-	this.observer = null;
+(function FBObserver(mutationHandler) {
+    this.target = document;
+    this.options = {
+        childList: true,
+        subtree: true
+    };
+    this.observer = null;
 
-	//BlockableHyperFeedItems.FriendsMemories = chrome.storage.sync.get()
+    chrome.storage.sync.get(BlockableHyperFeedItems, function (items) {
+        BlockableHyperFeedItems.SponsoredAds = items.SponsoredAds;
+        BlockableHyperFeedItems.Memories = items.Memories;
+        BlockableHyperFeedItems.AgoToday=items.AgoToday
+    });
 
-	if (window.MutationObserver && !this.observer)
-		this.observer = new MutationObserver(AdHandler);
+    if (window.MutationObserver && !this.observer)
+        this.observer = new MutationObserver(mutationHandler);
 
-	if (this.observer && this.target) {
-		try {
-			this.observer.observe(this.target, this.options);
-		} catch (e) {
-			console.log(e);
-		}
-	}
+    if (this.observer && this.target) {
+        try {
+            this.observer.observe(this.target, this.options);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
-	document.querySelectorAll("div[id^=hyperfeed_story_id]").forEach(function (n) {
-		hideSponsoredLink(n);
-	});
+    document.querySelectorAll("div[id^=hyperfeed_story_id]").forEach(function (n) {
+        hideHyperfeedLinks(n);
+    });
 
-	chrome.runtime.sendMessage({ "message": CountOfBlocked.toString() });
+    chrome.runtime.sendMessage({ "message": CountOfBlocked.toString() });
 
-})(mutationAdHandler);
+})(fbMutationHandler);
 
-function mutationAdHandler(mutations) {
-	try {
-		mutations.forEach(function (MR) {
-			//console.log("( mutation: " + MR.target.id + " )");
+function fbMutationHandler(mutations) {
+    try {
+        mutations.forEach(function (MR) {
+            //console.log("( mutation: " + MR.target.id + " )");
 
-			if (MR.target.matches("div[id^=hyperfeed_story_id]")) {
+            if (MR.target.matches("div[id^=hyperfeed_story_id]")) {
 
-				//console.log("( + hyperfeed: " + hs.id + " )");
+                //console.log("( + hyperfeed: " + hs.id + " )");
 
-				hideSponsoredLink(MR.target);
-			}
-		});
-	} catch (e) {
-		console.log(e);
-	}
+                hideHyperfeedLinks(MR.target);
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-function hideSponsoredLink(n) {
-	// hide nodes containing html sponsored stories, aka ads
-	var arrayOfa = n.querySelectorAll("a[href]");
+function hideHyperfeedLinks(n) {
+    // hide FB hyperfeed nodes containing html "sponsored stories", 
+    // aka ads and anything else I don't like
+    var arrayOfa = n.querySelectorAll("a[href]");
 
-	arrayOfa.forEach(function (a) {
-		if (a.innerText == "Sponsored") {
-			HideThis(n, "++ hiding ad ->");
-			IncrementBlockedCount(1);
-		}
+    arrayOfa.forEach(function (a) {
+        if (BlockableHyperFeedItems.SponsoredAds && a.innerText == "Sponsored")
+            HideThis(n, "++ hiding ad ->", 1);
 
-		if (BlockableHyperFeedItems.MemoriesOfFriends && a.href.indexOf("onthisday") > -1) {
-			HideThis(n, "++ hiding memory ->");
-			IncrementBlockedCount(2)
-		}
-	});
+        else if (BlockableHyperFeedItems.Memories && a.href.indexOf("onthisday") > -1)
+            HideThis(n, "++ hiding memory ->", 2);
+
+        else if (BlockableHyperFeedItems.AgoToday && a.href.indexOf("friendlyatheist") > -1) {
+            var ps = n.querySelectorAll("div.userContent > p")
+            ps.forEach(function (p) {
+                if (p.innerText.indexOf(" ago today") > -1)
+                    HideThis(n, "++ hiding ago today ->", 3);
+            });
+        }
+    });
 }
 
-function HideThis(n, prompt) {
-	if (!n.hidden) {
-		console.log("( " + prompt + n.id + " )");
-		n.hidden = true;
-	}
+function HideThis(n, prompt, type) {
+    if (!n.hidden) {
+        console.log("( " + prompt + n.id + " )");
+        n.hidden = true;
+        IncrementBlockedCount(type);
+    }
 }
 
 function IncrementBlockedCount(item) {
-	switch (item) {
-		case 1: // Ads
-			BlockedItemCounts.Ads++;
-			break;
+    switch (item) {
+        case 1: // SponsoredAds
+            BlockedItemCounts.SponsoredAds++;
+            break;
 
-		case 2: // Memories
-			BlockedItemCounts.Memories++;
-			break
-	}
+        case 2: // Memories
+            BlockedItemCounts.Memories++;
+            break;
 
-	CountOfBlocked = BlockedItemCounts.Ads + BlockedItemCounts.Memories;
+        case 3: // Old stories.
+            BlockedItemCounts.AgoToday++;
+            break;
 
-	chrome.runtime.sendMessage({ "message": CountOfBlocked.toString() });
+        default: 
+            break;
+    }
+
+    CountOfBlocked = BlockedItemCounts.SponsoredAds +
+        BlockedItemCounts.Memories +
+        BlockableHyperFeedItems.AgoToday;
+
+    chrome.runtime.sendMessage({ "message": CountOfBlocked.toString() });
 }
-
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-	for (key in changes) {
-		var storageChange = changes[key];
-		console.log('Storage key "%s" in namespace "%s" changed. ' +
-								'Old value was "%s", new value is "%s".',
-								key,
-								namespace,
-								storageChange.oldValue,
-								storageChange.newValue);
-	}
-});
